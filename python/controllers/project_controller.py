ccda23804439f5a263710992c264f8cdc52f34aa
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2020-12-16 18:18:29 -0300

    Refatoração do project_controller

--- python/controllers/project_controller.py
+++ python/controllers/project_controller.py
@@ -8,21 +8,20 @@ class ProjectController:
         self.projects = dict()
         self.current_id = 0
 
-    def add_project(self, name, description, base_text, mentor, mentor_aux, areas):
-        project_id = 1
-        for _ in self.projects:
-            project_id += 1
+    def add_project(self, name: str, description: str, base_text: str, mentor: object, mentor_aux: object, areas: list) -> int:
+        project_id = len(self.projects) + 1
         self.current_id = project_id
 
         project = Project(name, description, base_text,
                           mentor, mentor_aux, areas)
         self.projects[project_id] = project
+
         return project_id
 
-    def list_projects(self):
+    def list_projects(self) -> list:
         return list(self.projects.values())
 
-    def find_projects_by_state(self, state):
+    def find_projects_by_state(self, state: str) -> dict:
         found_projects = {}
 
         for key, val in self.projects.items():
@@ -33,7 +32,7 @@ class ProjectController:
 
         return found_projects
 
-    def find_projects_by_area(self, area):
+    def find_projects_by_area(self, area: list) -> dict:
         found_projects = {}
 
         for key, val in self.projects.items():
@@ -44,7 +43,7 @@ class ProjectController:
 
         return found_projects
 
-    def update_project_value(self, current_id, key, new_value):
+    def update_project_value(self, current_id: int, key: str, new_value: str) -> object:
         if current_id in self.projects:
             project = self.projects[current_id]
             setattr(project, key, new_value)
@@ -52,7 +51,7 @@ class ProjectController:
         else:
             raise Exception('Index Error')
 
-    def add_project_area(self, current_id, new_area):
+    def add_project_area(self, current_id: int, new_area: str) -> object:
         if current_id in self.projects:
             project = self.projects[current_id]
             project.areas.append(new_area)
@@ -60,7 +59,7 @@ class ProjectController:
         else:
             raise Exception('Index Error')
 
-    def remove_project_area(self, current_id, area):
+    def remove_project_area(self, current_id: int, area: str) -> str:
         if current_id in self.projects:
             project = self.projects[current_id]
             try:
@@ -71,7 +70,7 @@ class ProjectController:
         else:
             raise Exception('Index Error')
 
-    def remove_project(self, current_id):
+    def remove_project(self, current_id: int):
         if current_id in self.projects:
             project = self.projects[current_id]
             del self.projects[current_id]
@@ -79,7 +78,7 @@ class ProjectController:
         else:
             raise Exception('Index Error')
 
-    def find_project_by_mentor(self, email_mentor):
+    def find_project_by_mentor(self, email_mentor: str) -> dict:
         found_projects = {}
         for key, project in self.projects.items():
             if project.check_mentor_by_email(email_mentor):
